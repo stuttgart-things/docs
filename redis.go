ba64582a96b1d56bda51f2d1e62431f649f74da9
patrick hermann <47567770+patrick-hermann-sva@users.noreply.github.com>
2023-06-28 12:54:57 +0200

    Update redis.go

--- redis.go
+++ redis.go
@@ -4,10 +4,14 @@
 ```
 redis-cli -a ${REDIS_PASSWORD} # k8s redis inside pod
 redis-cli -h redis-pve.labul.sva.de -a ${REDIS_PASSWORD} # remote redis
-
 ```
 
 ## LIST KEYS
 ```
 KEYS *
+```
+
+## READ REDIS STREAM
+```
+XREAD COUNT 2 STREAMS redisqueue:yacht-revisionruns writers 0-0 0-0
 ```
