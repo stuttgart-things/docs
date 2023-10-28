220ac6e932b2970e2b78c0d501bd4c412d5aeb23
Patrick Hermann <patrick.hermann@sva.de>
2023-10-28 06:37:51 +0000

    added script for generate manifest markdown ot of files

--- hugo/create-content.sh
+++ hugo/create-content.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+# CREATE MANIFESTS AS MD
+
 dir="manifests"
 file="*.yaml"
 out="manifests.md"
 
 # CREATE HEADER
-echo -e "# /EXAMPLE-MANIFESTS\n" >> ${out}
+echo -e "# /MANIFESTS\n" >> ${out}
 
 for file in `cd ${dir};ls -1 ${file}` ;do
    echo ${file}
@@ -16,4 +18,21 @@ for file in `cd ${dir};ls -1 ${file}` ;do
    echo -e '\n```\n' >> ${out}
    echo '{{< /expand >}}' >> ${out}
    echo -e "\n" >> ${out}
+done
+
+# CREATE STHINGS AS MD
+
+dir="hugo"
+file="sthings-*.png"
+out="sthings.md"
+
+# CREATE HEADER
+echo -e "# /STHINGS\n" >> ${out}
+
+for file in `cd ${dir};ls -1 ${file}` ;do
+   echo ${file}
+   echo '{{< expand '\"${file}\"' "..." >}}' >> ${out}
+   echo -e "\n" >> ${out}
+   echo -e "![${file}](${file})" >> ${out}
+   echo '{{< /expand >}}' >> ${out}
 done
