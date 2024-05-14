7de5747141e79fabb1de45eec1ebf202914b8695
sina-schlatter <sina.schlatter@sva.de>
2024-05-14 09:28:40 +0000

    bugfix

--- hugo/create-sthings.sh
+++ hugo/create-sthings.sh
@@ -20,7 +20,6 @@ EOF
     # Modify width config in html files
     sed -i 's/max-width:100%/max-width:200%;width:1100px/g' "${dir}"/"$file"
     echo "String in file $file replaced."
-    cat "${dir}"/"$file"
 done
 
 # Copy hugo content
