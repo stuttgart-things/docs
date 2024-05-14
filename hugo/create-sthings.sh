3138cbfca0b86020f2d05bd05ab7aaa7f7981d7c
sina-schlatter <sina.schlatter@sva.de>
2024-05-14 07:36:59 +0000

    add sthings blog contenent

---
+++ hugo/create-sthings.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Define vars
+file="*html"
+dir="hugo"
+CURR_DATE=$(date)
+
+
+# Add new markdown lines at the beginning of the files
+ for file in `cd ${dir};ls -1 ${file}` ;do
+     cat <<EOF | cat - "$file" > temp && mv temp "$file"
++++
+date = $CURR_DATE
+draft = false
+title = $file
++++
+EOF
+    echo "New lines added to $file."
+
+    # Modify width config in html files
+    sed -i 's/max-width:100%/max-width:200%;width:1100px/g' "$file"
+    echo "String in file $file replaced."
+done
+
+# Copy hugo content
+cp -R hugo/*.html ./sthings/content/post
+cp hugo/sthings-logo.png ./sthings/themes/github-style/static/images/avatar.png
+
+# Modify theme files
+sed 's/container-lg/container-xl/g' ./sthings/layouts/partials/post.html
+# <div class="Box-body px-5 pb-5" style="z-index: 1" width=400%>
+
+
+# Create hugo toml
