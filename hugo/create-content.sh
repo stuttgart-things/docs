834fcc77bdc65a99c2425d3a9d05a80ab30deaab
patrick hermann <47567770+patrick-hermann-sva@users.noreply.github.com>
2024-05-21 19:45:26 +0200

    Update create-content.sh

--- hugo/create-content.sh
+++ hugo/create-content.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-echo "Creating hugo blog"
-hugo new site blog -f "yaml"
-git clone https://github.com/alex-shpak/hugo-book ./blog/themes/hugo-book
+SITE_NAME=$1
+
+echo "Creating hugo site ${SITE_NAME}"
+hugo new site ${SITE_NAME} -f "yaml"
+git clone https://github.com/alex-shpak/hugo-book ./${SITE_NAME}/themes/hugo-book
 
 # WORKAROUND
-cd ./blog/themes/hugo-book && git checkout v9 && cd -
+cd ./${SITE_NAME}/themes/hugo-book && git checkout v9 && cd -
 
-mkdir -p ./blog/content/docs
+mkdir -p ./${SITE_NAME}/content/docs
 
 # REWRITING DETAILS
 sed -i 's@<details><summary><b>@<details><summary>@g; s@</b></summary>@</summary>@g; s@<details><summary>@{{< expand "@g; s@</summary>@" >}}@g; s@</details>@{{< /expand >}}@g' *.md
 
 # COPY HUGO CONTENT
-cp -R hugo/config.yaml ./blog
-cp -R hugo/*.png ./blog/static
-cp -R hugo/*.ico ./blog/static
-cp -R hugo/*.md ./blog/content
-cp -R hugo/mermaid.json ./blog/assets
+cp -R hugo/config.yaml ./${SITE_NAME}
+cp -R hugo/*.png ./${SITE_NAME}/static
+cp -R hugo/*.ico ./${SITE_NAME}/static
+cp -R hugo/*.md ./${SITE_NAME}/content
+cp -R hugo/mermaid.json ./${SITE_NAME}/assets
 
 
 # CREATE MANIFESTS AS MD
@@ -65,7 +67,7 @@ for file in `cd ${dir};ls -1 ${file}` ;do
 done
 
 # CREATE STHINGS AS MD
-dir="hugo"
+dir=${SITE_NAME}
 file="sthings-*.png"
 out="sthings.md"
 
@@ -80,5 +82,5 @@ for file in `cd ${dir};ls -1 ${file}` ;do
    echo '{{< /expand >}}' >> ${out}
 done
 
-cp *.md ./blog/content/docs
-ls -lta blog/content/docs
+cp *.md ./${SITE_NAME}/content/docs
+ls -lta ${SITE_NAME}/content/docs
