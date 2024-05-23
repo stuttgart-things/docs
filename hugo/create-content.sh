ae6cc586cd235fa1276ece21ab373eae5f678714
sina-schlatter <sina.schlatter@sva.de>
2024-05-23 15:26:44 +0000

    update create-content.sh

--- hugo/create-content.sh
+++ hugo/create-content.sh
@@ -36,12 +36,12 @@ echo -e "# /MANIFESTS\n" >> ${out}
 # CREATE MANIFESTS INTO MD
 for file in `cd ${dir};ls -1 ${file}` ;do
    echo ${file}
-   echo '{{</* expand '\"${file}\"' "..." */>}}' >> ${out}
+   echo '{{< expand '\"${file}\"' "..." >}}' >> ${out}
    echo -e "\n" >> ${out}
    echo -e '```yaml' >> ${out}
    cat ${dir}/${file} >> ${out}
    echo -e '\n```\n' >> ${out}
-   echo '{{</* /expand */>}}' >> ${out}
+   echo '{{< expand >}}' >> ${out}
    echo -e "\n" >> ${out}
 done
 
@@ -58,12 +58,12 @@ echo -e "# /stageTime\n" >> ${out}
 # CREATE MANIFESTS INTO MD
 for file in `cd ${dir};ls -1 ${file}` ;do
    echo ${file}
-   echo '{{</* expand '\"${file}\"' "..." */>}}' >> ${out}
+   echo '{{< expand '\"${file}\"' "..." >}}' >> ${out}
    echo -e "\n" >> ${out}
    echo -e '```yaml' >> ${out}
    cat ${dir}/${file} >> ${out}
    echo -e '\n```\n' >> ${out}
-   echo '{{</* /expand */>}}' >> ${out}
+   echo '{{< expand >}}' >> ${out}
    echo -e "\n" >> ${out}
 done
 
@@ -77,10 +77,10 @@ echo -e "# /STHINGS\n" >> ${out}
 
 for file in `cd ${dir};ls -1 ${file}` ;do
    echo ${file}
-   echo '{{</* expand '\"${file}\"' "..." */>}}' >> ${out}
+   echo '{{< expand '\"${file}\"' "..." >}}' >> ${out}
    echo -e "\n" >> ${out}
    echo -e "![${file}](/static/${file})" >> ${out}
-   echo '{{</* /expand */>}}' >> ${out}
+   echo '{{< expand >}}' >> ${out}
 done
 
 cp *.md ./${SITE_NAME}/content/docs
