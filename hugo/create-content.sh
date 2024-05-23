7af2fef07cc2402db9b4fdf63100ea0327ba378b
sina-schlatter <sina.schlatter@sva.de>
2024-05-23 09:20:03 +0000

    fix syntax

--- hugo/create-content.sh
+++ hugo/create-content.sh
@@ -4,6 +4,7 @@ SITE_NAME=$1
 
 echo "Creating hugo site ${SITE_NAME}"
 hugo new site ${SITE_NAME} -f "yaml"
+rm sthings/hugo.toml
 git clone https://github.com/alex-shpak/hugo-book ./${SITE_NAME}/themes/hugo-book
 
 # WORKAROUND
@@ -12,10 +13,10 @@ git clone https://github.com/alex-shpak/hugo-book ./${SITE_NAME}/themes/hugo-boo
 mkdir -p ./${SITE_NAME}/content/docs
 
 # REWRITING DETAILS
-sed -i 's@<details><summary><b>@<details><summary>@g; s@</b></summary>@</summary>@g; s@<details><summary>@{{</* expand "@g; s@</summary>@" */>}}@g; s@</details>@{{</* /expand */>}}@g' *.md
+sed -i 's@<details><summary><b>@<details><summary>@g; s@</b></summary>@</summary>@g; s@<details><summary>@{{< expand "@g; s@</summary>@" >}}@g; s@</details>@{{< /expand >}}@g' *.md
 
 # COPY HUGO CONTENT
-cp -R hugo/config.yaml ./${SITE_NAME}
+#cp -R hugo/config.yaml ./${SITE_NAME}
 cp -R hugo/*.png ./${SITE_NAME}/static
 cp -R hugo/*.ico ./${SITE_NAME}/static
 cp -R hugo/*.md ./${SITE_NAME}/content
@@ -35,12 +36,12 @@ echo -e "# /MANIFESTS\n" >> ${out}
 # CREATE MANIFESTS INTO MD
 for file in `cd ${dir};ls -1 ${file}` ;do
    echo ${file}
-   echo '{{</* expand '\"${file}\"' "..." */>}}' >> ${out}
+   echo '{{< expand '\"${file}\"' "..." >}}' >> ${out}
    echo -e "\n" >> ${out}
    echo -e '```yaml' >> ${out}
    cat ${dir}/${file} >> ${out}
    echo -e '\n```\n' >> ${out}
-   echo '{{</* /expand */>}}' >> ${out}
+   echo '{{< /expand >}}' >> ${out}
    echo -e "\n" >> ${out}
 done
 
@@ -57,12 +58,12 @@ echo -e "# /stageTime\n" >> ${out}
 # CREATE MANIFESTS INTO MD
 for file in `cd ${dir};ls -1 ${file}` ;do
    echo ${file}
-   echo '{{</* expand '\"${file}\"' "..." */>}}' >> ${out}
+   echo '{{< expand '\"${file}\"' "..." >}}' >> ${out}
    echo -e "\n" >> ${out}
    echo -e '```yaml' >> ${out}
    cat ${dir}/${file} >> ${out}
    echo -e '\n```\n' >> ${out}
-   echo '{{</* /expand */>}}' >> ${out}
+   echo '{{< /expand >}}' >> ${out}
    echo -e "\n" >> ${out}
 done
 
@@ -76,11 +77,18 @@ echo -e "# /STHINGS\n" >> ${out}
 
 for file in `cd ${dir};ls -1 ${file}` ;do
    echo ${file}
-   echo '{{</* expand '\"${file}\"' "..." */>}}' >> ${out}
+   echo '{{< expand '\"${file}\"' "..." >}}' >> ${out}
    echo -e "\n" >> ${out}
    echo -e "![${file}](/static/${file})" >> ${out}
-   echo '{{</* /expand */>}}' >> ${out}
+   echo '{{< /expand >}}' >> ${out}
 done
 
 cp *.md ./${SITE_NAME}/content/docs
 ls -lta ${SITE_NAME}/content/docs
+
+# Modify and add hugo toml
+cat <<EOF | cat - hugo/hugo.yaml > temp && mv temp hugo/hugo.yaml
+baseURL: 'https://stuttgart-things.github.io/docs/'
+EOF
+
+cp -f hugo/hugo.yaml ./${SITE_NAME}
