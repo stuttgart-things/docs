c32a6ef1c0a8474763282d7ee24e372c7c62c62c
sina-schlatter <sina.schlatter@sva.de>
2024-05-23 08:13:09 +0000

    update syntax

--- hugo/create-content.sh
+++ hugo/create-content.sh
@@ -12,7 +12,7 @@ git clone https://github.com/alex-shpak/hugo-book ./${SITE_NAME}/themes/hugo-boo
 mkdir -p ./${SITE_NAME}/content/docs
 
 # REWRITING DETAILS
-sed -i 's@<details><summary><b>@<details><summary>@g; s@</b></summary>@</summary>@g; s@<details><summary>@{{< expand "@g; s@</summary>@" >}}@g; s@</details>@{{< /expand >}}@g' *.md
+sed -i 's@<details><summary><b>@<details><summary>@g; s@</b></summary>@</summary>@g; s@<details><summary>@{{< expand "@g; s@</summary>@" >}}@g; s@</details>@{{</* /expand */>}}@g' *.md
 
 # COPY HUGO CONTENT
 cp -R hugo/config.yaml ./${SITE_NAME}
@@ -40,7 +40,7 @@ for file in `cd ${dir};ls -1 ${file}` ;do
    echo -e '```yaml' >> ${out}
    cat ${dir}/${file} >> ${out}
    echo -e '\n```\n' >> ${out}
-   echo '{{< /expand >}}' >> ${out}
+   echo '{{</* /expand */>}}' >> ${out}
    echo -e "\n" >> ${out}
 done
 
@@ -62,7 +62,7 @@ for file in `cd ${dir};ls -1 ${file}` ;do
    echo -e '```yaml' >> ${out}
    cat ${dir}/${file} >> ${out}
    echo -e '\n```\n' >> ${out}
-   echo '{{< /expand >}}' >> ${out}
+   echo '{{</* /expand */>}}' >> ${out}
    echo -e "\n" >> ${out}
 done
 
@@ -79,7 +79,7 @@ for file in `cd ${dir};ls -1 ${file}` ;do
    echo '{{< expand '\"${file}\"' "..." >}}' >> ${out}
    echo -e "\n" >> ${out}
    echo -e "![${file}](/static/${file})" >> ${out}
-   echo '{{< /expand >}}' >> ${out}
+   echo '{{</* /expand */>}}' >> ${out}
 done
 
 cp *.md ./${SITE_NAME}/content/docs
